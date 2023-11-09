160df35aa46f9f4e01b869aa3d411fa224251cdb
진찬호 <chanhojin94@kakao.com>
2023-11-09 22:05:00 +0900

    Feat: User CRUD ing

---
+++ src/reports/report.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: Date;
+
+  @Column()
+  mileage: number;
+
+  @Column()
+  longitude: number;
+
+  @Column()
+  latitude: number;
+
+  @Column()
+  price: number;
+}
