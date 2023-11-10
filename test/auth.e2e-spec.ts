1ea4c8ff0c24fd3796757e38d81a0f64e6709ffd
진찬호 <chanhojin94@kakao.com>
2023-11-10 22:00:35 +0900

    before env

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication App', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a singup request', () => {
+    const email = 'assdf1234@asdf.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
